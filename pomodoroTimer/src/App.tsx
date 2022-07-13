7d56b93466cbcf3313e23b823718942401a91e85
mateus-cristian <mateus_cristian@outlook.com>
2022-07-13 14:18:47 -0300

    [OK]style global

--- pomodoroTimer/src/App.tsx
+++ pomodoroTimer/src/App.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button/Button";
+import { GlobalStyle } from "./styles/global";
+import { DefaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <ThemeProvider theme={DefaultTheme}>
+      <Button variant="primary" />
+      <Button variant="danger" />
+      <Button variant="sucess" />
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
---
+++ pomodoroTimer/src/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ButtonContainer, ButtonVariant } from "./Button.styles";
+
+interface ButtonProps {
+  variant?: ButtonVariant;
+}
+
+export function Button({ variant = "primary" }: ButtonProps) {
+  return (
+    <>
+      <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+    </>
+  );
+}
