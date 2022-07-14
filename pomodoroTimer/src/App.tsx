6c5e2214a957885f60a100c45779290fba260f6a
mateus-cristian <mateus_cristian@outlook.com>
2022-07-13 22:45:16 -0300

    [OK]Create home and routes

--- pomodoroTimer/src/App.tsx
+++ pomodoroTimer/src/App.tsx
@@ -1,15 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button/Button";
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import { DefaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
-      <Button variant="primary" />
-      <Button variant="danger" />
-      <Button variant="sucess" />
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
